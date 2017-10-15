26e108f6dadb014f254b2354caa214f94305ad67
chenfeng-SuperPC <cfryan1990@gmail.com>
2017-10-15 23:00:26 +0800

    google目前主推方式 通过cmake

--- NDKHelloWorld/app/src/main/java/com/cfryan/ndkhelloworld/MainActivity.java
+++ NDKHelloWorld/app/src/main/java/com/cfryan/ndkhelloworld/MainActivity.java
@@ -3,7 +3,6 @@ package com.cfryan.ndkhelloworld;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
